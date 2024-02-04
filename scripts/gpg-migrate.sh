7b5bcda333c97b5427fda0378d6fe094b404ca18
Kafva <36083692+Kafva@users.noreply.github.com>
2024-02-04 18:25:10 +0100

    gpg-migrate fixes

--- scripts/gpg-migrate.sh
+++ scripts/gpg-migrate.sh
@@ -15,36 +15,38 @@ fi
 
 GPG_STORE="$1"
 NAME="$2"
-AGE_STORE="$PWD/.passage"
+AGE_STORE="$PWD/kage-store/$NAME"
 
-mkdir -p "$AGE_STORE/store/$NAME"
+mkdir -p "$AGE_STORE/store"
 
 # Generate a new key
 KEY="$(age-keygen)"
 PUBKEY=$(age-keygen -y <<< "$KEY")
 
 # Save it, passphrase encrypted
-age -p -a <<< "$KEY" > "$AGE_STORE/.$NAME"
+# We only want one identity to have access to each store
+age -p -a <<< "$KEY" > "$AGE_STORE/identities"
 
-# Add it to the list of identities
-cat "$AGE_STORE/.$NAME" >> "$AGE_STORE/identities"
+# Mark it as the recipient
+echo "$PUBKEY" >> "$AGE_STORE/store/.age-recipients"
 
-# Use it to encrypt/decrypt under store/$NAME
-echo "$PUBKEY" >> "$AGE_STORE/store/$NAME/.age-recipients"
+GPG_ID=$(cat $GPG_STORE/.gpg-id)
 
 while read -r gpgfile; do
     _gpgfile=$(sed "s@$GPG_STORE/@@" <<< "$gpgfile")
-    agefile="$AGE_STORE/store/$NAME/${_gpgfile%%.gpg}.age"
+    agefile="$AGE_STORE/store/${_gpgfile%%.gpg}.age"
     mkdir -p "$(dirname $agefile)"
 
     info "$_gpgfile"
-    gpg -r "$(cat $GPG_STORE/.gpg-id)" -d "$gpgfile" |
+    gpg -q -r "$GPG_ID" -d "$gpgfile" |
         age -r "$PUBKEY" -o "$agefile" -
 
 done < <(find "$GPG_STORE" -type f -name '*.gpg')
 
 tree -a --noreport $AGE_STORE
 
+[ -d $GPG_STORE/.git ] && cp -r $GPG_STORE/.git $AGE_STORE/store
+
 info "Test decryption"
-agefile="$(find "$AGE_STORE/store/$NAME" -type f -name '*.age' | head -n1)"
+agefile="$(find "$AGE_STORE/store" -type f -name '*.age' | head -n1)"
 age -i $AGE_STORE/identities -d $agefile
