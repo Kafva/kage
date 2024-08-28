beef0e889d3faee7d4798b3dc1a6fa837231c0ad
Kafva <kafva.one@gmail.com>
2024-08-28 06:32:02 +0200

    commit preview

--- ios/src/views/HistoryView.swift
+++ ios/src/views/HistoryView.swift
@@ -2,37 +2,52 @@ import OSLog
 import SwiftUI
 
 struct HistoryView: View {
-    @AppStorage("remote") private var remote: String = ""
     @EnvironmentObject var appState: AppState
-    @State private var messages = [CommitInfo]()
+    @State private var commits = [CommitInfo]()
 
-    // TODO: Preview complete message and rev
     var body: some View {
-        return List(messages) { m in
-            VStack(alignment: .leading) {
-                Text(m.summary).font(.body)
-                Text(m.date).foregroundColor(.gray).font(.caption)
-                if m.revstr.count != 40 {
-                    // The `revstr` will be a prettified string for the
-                    // remote HEAD instead of a hash, highlight it.
-                    Text(m.revstr)
-                        .foregroundColor(.accentColor)
-                        .bold()
-                        .font(.caption)
+        return List(commits) { commit in
+            NavigationLink(destination: HistoryItemView(commit: commit)) {
+                VStack(alignment: .leading) {
+                    Text(commit.summary).font(.body)
+                    Text(commit.date).foregroundColor(.gray).font(.caption)
+                    if commit.revstr.count != 40 {
+                        // The `revstr` will be a prettified string for the
+                        // remote HEAD instead of a hash, highlight it.
+                        Text(commit.revstr)
+                            .foregroundColor(.accentColor)
+                            .bold()
+                            .font(.caption)
+                    }
                 }
+                .lineLimit(1)
+                .truncationMode(.tail)
             }
-            .lineLimit(1)
-            .truncationMode(.tail)
         }
         .listStyle(.plain)
         .scrollContentBackground(.hidden)
         .onAppear {
             do {
-                messages = try Git.log()
+                commits = try Git.log()
             }
             catch {
                 G.logger.error("\(error.localizedDescription)")
             }
         }
+    }
+}
+
+private struct HistoryItemView: View {
+    let commit: CommitInfo
+
+    var body: some View {
+        let revColor: Color = commit.revstr.count != 40 ? .accentColor : .gray
+        VStack(alignment: .leading, spacing: 5) {
+            Text(commit.summary).font(.body).padding(.top, G.screenHeight * 0.1)
+            Text(commit.date).foregroundColor(.gray).font(.caption).lineLimit(1)
+            Text(commit.revstr).foregroundColor(revColor).font(.caption).lineLimit(1)
+            Spacer()
+        }
+        .padding([.leading, .trailing], 20)
     }
 }
