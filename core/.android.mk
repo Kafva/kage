f347c5fe7a5347e14a14dfe7bf9e62aceeac9210
Kafva <kafva.one@gmail.com>
2024-10-13 14:05:34 +0200

    Cleanup makefiles

---
+++ core/.android.mk
@@ -0,0 +1,42 @@
+ifeq ($(NDK_HOME),)
+$(error NDK_HOME is unset)
+endif # NDK_HOME
+
+SOURCE_ROOT = $(CURDIR)/../android
+ANDROID_TARGET_ARCH ?= aarch64
+
+CARGO_CRATE_TYPE = dylib
+LIB = libkage_core.so
+
+CARGO_TARGET = $(ANDROID_TARGET_ARCH)-linux-android
+
+ifeq ($(ANDROID_TARGET_ARCH),aarch64)
+DIST = $(SOURCE_ROOT)/src/main/jniLibs/arm64-v8a
+
+else ifeq ($(ANDROID_TARGET_ARCH),x86_64)
+DIST = $(SOURCE_ROOT)/src/main/jniLibs/x86_64
+
+else
+$(error Unsupported architecture: '$(ANDROID_TARGET_ARCH)')
+
+endif # ANDROID_TARGET_ARCH
+
+ifeq ($(shell uname -s),Darwin)
+# There is no arm64 macOS toolchain
+ANDROID_HOST_ARCH = darwin-x86_64
+else
+ANDROID_HOST_ARCH = linux-$(shell uname -m)
+endif
+
+ANDROID_BINDIR = $(NDK_HOME)/toolchains/llvm/prebuilt/$(ANDROID_HOST_ARCH)/bin
+ANDROID_CC = $(ANDROID_BINDIR)/$(ANDROID_TARGET_ARCH)-linux-android35-clang
+ANDROID_AR = $(ANDROID_BINDIR)/llvm-ar
+
+export CC_$(subst -,_,$(CARGO_TARGET)) = $(ANDROID_CC)
+export AR_$(subst -,_,$(CARGO_TARGET)) = $(ANDROID_AR)
+
+# $NDK_HOME is not expandable inside `.cargo/config.toml` so we provide it from
+# here instead for now.
+# https://github.com/rust-lang/cargo/issues/10789
+CARGO_RUSTC_FLAGS += --config "target.$(CARGO_TARGET).linker='$(ANDROID_CC)'"
+CARGO_RUSTC_FLAGS += --config "target.$(CARGO_TARGET).ar='$(ANDROID_AR)'"
---
+++ core/.ios.mk
@@ -0,0 +1,32 @@
+# Xcode sets SOURCE_ROOT, PLATFORM_DISPLAY_NAME, CONFIGURATION etc.
+SOURCE_ROOT ?= $(CURDIR)/../ios
+PLATFORM_DISPLAY_NAME ?= iOS Simulator
+CONFIGURATION ?= Debug
+
+CARGO_CRATE_TYPE = staticlib
+DIST = $(SOURCE_ROOT)/dist
+LIB = libkage_core.a
+
+ifeq ($(PLATFORM_DISPLAY_NAME),iOS Simulator)
+
+ifeq ($(shell uname -m),x86_64)
+CARGO_TARGET = x86_64-apple-ios
+else
+CARGO_TARGET = aarch64-apple-ios-sim
+endif
+
+else ifeq ($(PLATFORM_DISPLAY_NAME),iOS)
+CARGO_TARGET = aarch64-apple-ios
+
+else
+$(error Unsupported platform)
+
+endif # PLATFORM_DISPLAY_NAME
+
+ifeq ($(CONFIGURATION),Release)
+# Disable default debug_logs feature for release builds
+CARGO_RUSTC_FLAGS += --no-default-features
+endif
+
+# Library path must be set explicitly when the build is triggered from Xcode
+export LIBRARY_PATH = /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib
