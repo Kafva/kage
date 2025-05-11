401f55fa61580d168bd5556223ac4d8931ed948f
Kafva <kafva.one@gmail.com>
2025-05-11 10:02:55 +0200

    ios: Use TARGET_DEVICE_PLATFORM_NAME over PLATFORM_DISPLAY_NAME

--- core/.ios.mk
+++ core/.ios.mk
@@ -1,13 +1,13 @@
-# Xcode sets: PLATFORM_DISPLAY_NAME and CONFIGURATION.
+# Xcode sets: TARGET_DEVICE_PLATFORM_NAME and CONFIGURATION.
 # These need to be passed from the build script!
-PLATFORM_DISPLAY_NAME ?= iOS Simulator
+TARGET_DEVICE_PLATFORM_NAME ?= iphonesimulator
 CONFIGURATION ?= Debug
 
 CARGO_CRATE_TYPE = staticlib
 DIST = $(CURDIR)/../ios/dist
 LIB = libkage_core.a
 
-ifeq ($(PLATFORM_DISPLAY_NAME),iOS Simulator)
+ifeq ($(TARGET_DEVICE_PLATFORM_NAME),iphonesimulator)
 
 ifeq ($(shell uname -m),x86_64)
 CARGO_TARGET = x86_64-apple-ios
@@ -15,13 +15,13 @@ else
 CARGO_TARGET = aarch64-apple-ios-sim
 endif
 
-else ifeq ($(PLATFORM_DISPLAY_NAME),iOS)
+else ifeq ($(TARGET_DEVICE_PLATFORM_NAME),iphoneos)
 CARGO_TARGET = aarch64-apple-ios
 
 else
 $(error Unsupported platform)
 
-endif # PLATFORM_DISPLAY_NAME
+endif # TARGET_DEVICE_PLATFORM_NAME
 
 ifeq ($(CONFIGURATION),Release)
 # Disable default debug_logs feature for release builds
